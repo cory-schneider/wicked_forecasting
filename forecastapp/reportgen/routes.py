d2be0d3ea51036054c9c66ce7e8abbc3e4c65793
Cory Schneider <corybschneider@gmail.com>
2020-06-08 16:57:02 -0400

    Add PDCN Merge - front end work

--- forecastapp/reportgen/routes.py
+++ forecastapp/reportgen/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
 from forecastapp.reportgen.forms import ReportGeneratorForm
 
 reportgen = Blueprint('reportgen', __name__)
 
 @reportgen.route("/report_generator")
+@login_required
 def report_generator():
     form = ReportGeneratorForm()
     return render_template('report_generator.html', title='Forecast Helper Report Generator',
